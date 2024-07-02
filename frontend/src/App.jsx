315a1bff096c5f1d789cc4cc1d9da17af863f358
Adnan Tahir <adnantabda@gmail.com>
2024-07-02 22:28:18 +0300

    feat: create basic App component with Home component integration
    - Imported App.css for styling
    - Added Home component from ./components/Home
    - Created App component with a div wrapper and included Home component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css" 
+import { Home } from "./components/Home";
+export default function App(){
+    return (
+        <div className="app">
+            <Home />
+        </div>
+    )
+}
