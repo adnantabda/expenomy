2b4c566bff3e2d3967d426d88ec13d8817b8e7ac
Adnan Tahir <adnantabda@gmail.com>
2024-07-10 19:03:06 +0300

    redesign dashboard

--- frontend/src/components/LoginDashboard.jsx
+++ frontend/src/components/LoginDashboard.jsx
@@ -1,99 +1,105 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { url } from "./Login";
+import './LoginDashboard.css'
+
+function TodayCal() {
+  const hashDay = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  };
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const date = today.getDate();
+
+  return (
+    <div className="date">
+      <p>
+        <span>{year}</span>
+        <span>{month}</span>
+        <span>{date}</span>
+      </p>
+      <p className='day'>{hashDay[today.getDay()]}</p>
+    </div>
+  );
+}
+function Name({name}){
+  return (
+    <div className='name'>
+      <h3>{name}</h3>
+    </div>
+  )
+}
+
+function Header(){
+  return (
+    <div className='header'>
+      <TodayCal></TodayCal>
+      <Name name={"Adnan Tahir"}></Name>
+    </div>
+  )
+}
 
 
-const tagLine = ["Record Your finance", "Take a close look at your Expenses", "Have a financial freedom"]
+function Nav(){
+  return (
+    <div className='nav-b'>
+      <div>Expenses</div>
+      <div>Budget</div>
+      <div>Income</div>
+      <div>Stats</div>
+    </div>
+  )
+}
 
-function Header({name}){
-    const randomTag =Math.floor(Math.random() * tagLine.length)
-    // console.log(randomTag)
-    return (
-        <div>
-            <h2>{name}</h2>
-            <p>{tagLine[randomTag]}</p>
-        </div>
-    )
+function CenterDash(){
+  const Li = []
+  return (
+    <div>
+      <ul>
+        {Li.map((l)=><li className='list-item'>{l}</li>)}
+      </ul>
+      
+    </div>
+  )
+}
+
+function LeftDash(){
+  return (
+    <div>
+   </div>
+  )
+}
+
+function RightDash(){
+  return (
+    <div>
+    </div>
+  )
 }
 
 
 
-function Summary(){
-    return (
-        <div className="summary">
-            <div className="total">
-                <h3>Total spend</h3>
-                <p>$120</p>
-            </div>
-            <div className="top-most-expense">
-                <h3>Most recent spend</h3>
-                <p>$30 <span>Food</span></p>
-            </div>
-            <div>
-                <h3>Total Income</h3>
-                <p>$500</p>
-            </div>
-        </div>
-    )
-}
-
-function AddChoice(){
-    return (
-        <div className="add-choice">
-            <button>Add Expense</button>
-            <button>Add Category</button>
-            <button>View Categories</button>
-            <button>View Status</button>
-        </div>
-    )
-}
-
-function ListExpenses({expenses}){
-    return (
-        <div className="expenses">
-            <h2>Expenses</h2>
-            <div>
-                <ul>
-                    {expenses.map((expense)=><li key={expense.id}>{expense.description} {expense.id}</li>)}
-                </ul>
-            </div>
-        </div>
-    )
-
+function Dashboard(){
+  return (
+    <div className='sub-dashboard'>
+      <LeftDash></LeftDash>
+      <CenterDash></CenterDash>
+      <RightDash></RightDash>
+    </div>
+  )
 }
 
 export default function SuccessLogin() {
-    const [expenses, setExpenses] = useState(["name", "Twp", "TE"])
-    const [name , setName] = useState("")
-    const location = useLocation();
-    const {username, access_token} = location.state || {};
-
-    useEffect(()=>{
-        console.log(access_token)
-        console.log(username)
-        fetch(`${url}/${username}/dashboard`, {
-            method: "GET",
-            headers: {
-                'Authorization': `Bearer ${access_token}`,
-            }
-        }).then((response)=>response.json()).then((data)=>{
-            if (data.expenses == undefined){
-                expenses = ['name', 1, 4]
-            }else{
-                setExpenses(data.expenses)
-                setName(username)
-            }
-        })
-
-    }, [])
-
-
   return (
-    <div className="dashboard">
-        <Header name={name}/>
-        <Summary />
-        <AddChoice />
-        <ListExpenses expenses={expenses}/>
+    <div className='dashboard'>
+      <Header></Header>
+      <Nav></Nav>
+      <Dashboard></Dashboard>
     </div>
   );
 }
